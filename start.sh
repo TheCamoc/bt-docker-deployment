f54bedc61fc1e2c652afa9dc54483b406b5107b2
Chloe <chloe.pr@pm.me>
2023-01-03 14:28:27 +0100

    remove makemigrations

--- start.sh
+++ start.sh
@@ -3,7 +3,6 @@
 if [ -e /database/secret.key ]
 then
   echo 'Secret Key found.'
-  
 else
   echo 'Creating new Secret Key.'
   python -c 'from django.core.management.utils import get_random_secret_key; print(get_random_secret_key())' > /database/secret.key
@@ -11,12 +10,10 @@ fi
 
 if [ -e /database/db.sqlite3 ]
 then
-  echo 'Existing Database found, trying migrations.'
-  python manage.py makemigrations
+  echo 'Existing Database found, trying to apply migrations.'
   python manage.py migrate
 else
-  echo 'Creating new Admin user in new Database.'
-  python manage.py makemigrations
+  echo 'Creating new Database with default Admin user.'
   python manage.py migrate
   export DJANGO_SUPERUSER_PASSWORD="kV9lkxVVwYi(Msf;(x9U"
   python manage.py createsuperuser --username admin --email admin@example.com --noinput
